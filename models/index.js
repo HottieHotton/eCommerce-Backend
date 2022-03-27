db31f90211d2eec2bde12de5b9ee95166db2a07c
Bhotton25 <bhotton25@gmail.com>
2022-03-26 19:20:53 -0600

    Added model details

--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'catagory_id'
+});
 
 // Categories have many Products
+Catagory.hasMany(Product, {
+  foreignKey: 'catagory_id',
+  onDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'ProductTag'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'ProductTag'
+});
 
 module.exports = {
   Product,
