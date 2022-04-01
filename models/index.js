81a6edddafce842313a67387930df67fd7e5def9
Bhotton25 <bhotton25@gmail.com>
2022-03-31 18:28:47 -0600

    Fix spelling error

--- models/index.js
+++ models/index.js
@@ -10,7 +10,7 @@ Product.belongsTo(Category, {
 });
 
 // Categories have many Products
-Catagory.hasMany(Product, {
+Category.hasMany(Product, {
   foreignKey: 'catagory_id',
   onDelete: 'CASCADE'
 });
